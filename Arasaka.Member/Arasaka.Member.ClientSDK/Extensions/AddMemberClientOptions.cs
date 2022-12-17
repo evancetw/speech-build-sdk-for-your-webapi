9d3c0352d254215bbda16e7742424c382148eee9
evancetw <evancekao@outlook.com>
2022-12-17 11:31:59 +0800

    feat: 新增服務註冊

---
+++ Arasaka.Member/Arasaka.Member.ClientSDK/Extensions/AddMemberClientOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arasaka.Member.ClientSDK.Extensions
+{
+    public class AddMemberClientOptions
+    {
+        public string BaseUrl { get; set; }
+
+        public MemberClientOptions ClientOptions { get; set; }
+    }
+}
---
+++ Arasaka.Member/Arasaka.Member.ClientSDK/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Arasaka.Member.ClientSDK.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static ServiceCollection AddMemberClient(
+            this ServiceCollection service,
+            Action<AddMemberClientOptions> setOption = null)
+        {
+            var option = new AddMemberClientOptions();
+            setOption?.Invoke(option);
+
+            service.AddSingleton<MemberClient>(sp =>
+            {
+                return new MemberClient(
+                    option.BaseUrl,
+                    sp.GetRequiredService<HttpClient>(),
+                    sp.GetRequiredService<ILogger<MemberClient>>(),
+                    option.ClientOptions
+                    );
+            });
+
+            return service;
+        }
+    }
+}
